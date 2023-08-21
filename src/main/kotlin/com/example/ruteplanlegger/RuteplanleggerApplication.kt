7b9b1913d30b2b51527aff44911051475612915b
RagnhildK <ragnhildk98@gmail.com>
2023-08-21 10:09:21 +0200

    Add existing project files prior to the push to GitHub.

---
+++ src/main/kotlin/com/example/ruteplanlegger/RuteplanleggerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ruteplanlegger
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RuteplanleggerApplication
+
+fun main(args: Array<String>) {
+	runApplication<RuteplanleggerApplication>(*args)
+}
