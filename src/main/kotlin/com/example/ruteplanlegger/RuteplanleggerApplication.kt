4216715fd08da8b5e72a7797cccc32e1eae9187a
RagnhildK <41580514+RagnhildK@users.noreply.github.com>
2023-08-22 10:22:45 +0200

    create endpoint in controller (#1)

--- src/main/kotlin/com/example/ruteplanlegger/RuteplanleggerApplication.kt
+++ src/main/kotlin/com/example/ruteplanlegger/RuteplanleggerApplication.kt
@@ -6,6 +6,9 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class RuteplanleggerApplication
 
+
 fun main(args: Array<String>) {
 	runApplication<RuteplanleggerApplication>(*args)
 }
+
+
