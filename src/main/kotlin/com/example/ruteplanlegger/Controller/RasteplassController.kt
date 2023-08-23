c97bfd04eec15bc8b22096e207722fbfa5313218
RagnhildK <ragnhildk98@gmail.com>
2023-08-23 15:29:57 +0200

    allow all origins, add egenskaper to SVV call and limit to 50 rasteplasser

--- src/main/kotlin/com/example/ruteplanlegger/Controller/RasteplassController.kt
+++ src/main/kotlin/com/example/ruteplanlegger/Controller/RasteplassController.kt
@@ -1,12 +1,14 @@
 package com.example.ruteplanlegger.Controller
 
-import com.example.ruteplanlegger.service.RasteplasserService
+import com.example.ruteplanlegger.Service.RasteplasserService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/rasteplasser")
+@CrossOrigin(origins = ["*"])
 class Rasteplasser(val rasteplasserService: RasteplasserService) {
 
     @GetMapping
