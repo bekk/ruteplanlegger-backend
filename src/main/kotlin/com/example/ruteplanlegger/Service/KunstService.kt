934d64e81ff26059aba34c8557c19a3d87d5ff83
Purusoth Thushyanthan <purusoth.thushyanthan@bekk.no>
2023-08-31 10:21:16 +0200

    refakturere kode og hente kunst med type

--- src/main/kotlin/com/example/ruteplanlegger/Service/KunstService.kt
+++ src/main/kotlin/com/example/ruteplanlegger/Service/KunstService.kt
@@ -6,32 +6,25 @@ import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 
 fun retrieveTittelTypeandId(data: JsonNode): List<Kunst> {
-    val listOfProcessedKunst: MutableList<Kunst> = mutableListOf()
-    val listOfKunst = data["objekter"]
-    for (kunst in listOfKunst) {
-        val id = kunst["id"].asInt()
-        var tittel = ""
-        var type = ""
-        val egenskaper = kunst["egenskaper"]
-        for (egenskap in egenskaper) {
-            if (egenskap["id"].asInt() == 1733) {
-                tittel = egenskap["verdi"].asText()
-            }
-            if (egenskap["id"].asInt() == 1101) {
-                type = egenskap["verdi"].asText()
-            }
-        }
-        val kunst = Kunst(id, tittel, type)
-        listOfProcessedKunst.add(kunst)
-    }
-    return listOfProcessedKunst
-}
 
+    return data["objekter"].map { kunst ->
+        val id = kunst["id"].asInt()
+        val tittel = kunst["egenskaper"].find {
+            it["id"].asInt() == 1733
+        }?.get("verdi")?.asText() ?: ""
+        val type = kunst["egenskaper"].find {
+            it["id"].asInt() == 1101
+        }?.get("verdi")?.asText() ?: ""
+        Kunst(id, tittel, type)
+    }
+
+}
     @Service
     class KunstService(val webClient: WebClient.Builder) {
-
         fun getKunstOgUtsmykking(): List<Kunst>? {
-            val apiURL = "https://nvdbapiles-v3.atlas.vegvesen.no/vegobjekter/19?&inkluder=egenskaper&antall=30"
+            val apiURL =
+                "https://nvdbapiles-v3.atlas.vegvesen.no/vegobjekter/19?inkluder=egenskaper&antall=30&egenskap=(1101!=null)"
+
             val response = webClient.baseUrl(apiURL).build().get().retrieve().bodyToMono(JsonNode::class.java).block()
             return if (response is JsonNode) retrieveTittelTypeandId(response) else emptyList()
         }
