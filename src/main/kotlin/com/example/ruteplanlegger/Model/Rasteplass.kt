ed26f0f06600e7c33c1ede822270e9c7ef4baeeb
emilielr <43408175+emilielr@users.noreply.github.com>
2023-09-06 15:15:40 +0200

    Legge til koordinater på rasteplass (#12)
    * add geometri and update coorinate system in api request
    
    * add geometri and coordinate system 4326 to rasteplass call
    
    * add geomrti to Rasteplass
    
    ---------
    
    Co-authored-by: RagnhildK <ragnhildk98@gmail.com>

--- src/main/kotlin/com/example/ruteplanlegger/Model/Rasteplass.kt
+++ src/main/kotlin/com/example/ruteplanlegger/Model/Rasteplass.kt
@@ -1,8 +1,19 @@
 package com.example.ruteplanlegger.Model
 
+data class Geometri(
+    val type: String = "Point",
+    val coordinates: LatLong
+)
+
+data class LatLong(
+    val long: Double,
+    var lat: Double
+)
+
 data class Rasteplass(
     val id: Int = 0,
     val navn: String = "",
     val vegkategori: String = "",
     val vegnummer: Int = 0,
+    val geometri: Geometri
 )
--- src/main/kotlin/com/example/ruteplanlegger/Service/RasteplasserService.kt
+++ src/main/kotlin/com/example/ruteplanlegger/Service/RasteplasserService.kt
@@ -1,13 +1,31 @@
 package com.example.ruteplanlegger.Service
 
+import com.example.ruteplanlegger.Model.Geometri
+import com.example.ruteplanlegger.Model.LatLong
 import com.example.ruteplanlegger.Model.Rasteplass
 import com.fasterxml.jackson.databind.JsonNode
+import org.locationtech.jts.geom.Geometry
+import org.locationtech.jts.io.WKTReader
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 
+fun createGeometri(geometri: JsonNode): Geometri {
+    val wktNode = geometri["wkt"]
+    val wkt = wktNode.asText()
+    val wktReader = WKTReader()
+    val geometry: Geometry = wktReader.read(wkt)
+    val centroid =
+        geometry.centroid //gir et Point som er ca. i midten av en Linestring/Polygon
+    val lat: Double = centroid.coordinate.y
+    val long: Double = centroid.coordinate.x
+    val latLongObject = LatLong(long, lat)
+
+    return Geometri(type = "Point", coordinates = latLongObject)
+}
 
 fun createMinimalResteplassObject(data: JsonNode): List<Rasteplass> {
     return data["objekter"].map { rasteplass ->
+
         val id = rasteplass["id"].asInt()
         val navn = rasteplass["egenskaper"]
             .find { it["id"].asInt() == 1074 }
@@ -29,8 +47,9 @@ fun createMinimalResteplassObject(data: JsonNode): List<Rasteplass> {
             ?.asInt()
             ?: 0
 
+        val geometry = createGeometri(rasteplass["geometri"])
 
-        Rasteplass(id = id, navn = navn, vegkategori = vegkategori, vegnummer = vegnummer)
+        Rasteplass(id = id, navn = navn, vegkategori = vegkategori, vegnummer = vegnummer, geometri = geometry)
     }
 }
 
@@ -38,7 +57,7 @@ fun createMinimalResteplassObject(data: JsonNode): List<Rasteplass> {
 class RasteplasserService(val webClient: WebClient.Builder) {
     fun getAllRasteplasser(): List<Rasteplass>? {
         val apiURL =
-            "https://nvdbapiles-v3.atlas.vegvesen.no/vegobjekter/39?antall=50&inkluder=vegsegmenter,egenskaper&inkluder_egenskaper=basis"
+            "https://nvdbapiles-v3.atlas.vegvesen.no/vegobjekter/39?antall=50&inkluder=vegsegmenter,egenskaper,geometri&inkluder_egenskaper=basis&srid=4326"
         val response = webClient.baseUrl(apiURL).build().get().retrieve().bodyToMono(JsonNode::class.java).block()
         return if (response is JsonNode) createMinimalResteplassObject(response) else emptyList()
     }
