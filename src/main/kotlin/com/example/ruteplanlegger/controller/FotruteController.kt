35a87ac5b1a528c98fec21b0a3263c365e227b87
RagnhildK <41580514+RagnhildK@users.noreply.github.com>
2023-09-12 14:30:22 +0200

    Remove print (#19)
    * remove print
    
    * edit fotrute controller

--- src/main/kotlin/com/example/ruteplanlegger/controller/FotruteController.kt
+++ src/main/kotlin/com/example/ruteplanlegger/controller/FotruteController.kt
@@ -3,17 +3,17 @@ package com.example.ruteplanlegger.controller
 import com.example.ruteplanlegger.model.Fotrute
 import com.example.ruteplanlegger.service.FotruteService
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/fotruter")
 @CrossOrigin(origins = ["*"])
 class FotruteController(val fotruteService: FotruteService) {
 
-
-    @RequestMapping("/fotruter")
+    @GetMapping
     fun getFotruter(): List<Fotrute> {
-
         return fotruteService.getFotruter()
 
 
