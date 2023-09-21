acb91f256c914df8663c0180c464108db55566d4
Purusoth Thushyanthan <purusoth.thushyanthan@bekk.no>
2023-09-21 10:54:03 +0200

    try to change format in csv file to geojson

--- src/main/kotlin/com/example/ruteplanlegger/service/FotruteService.kt
+++ src/main/kotlin/com/example/ruteplanlegger/service/FotruteService.kt
@@ -4,6 +4,7 @@ import com.example.ruteplanlegger.model.Fotrute
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.api.rows
 import org.jetbrains.kotlinx.dataframe.io.read
+import org.jetbrains.kotlinx.dataframe.io.readCSV
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,14 +14,14 @@ class FotruteService {
         var df = DataFrame.read("3-fotruter.csv")
         val listofFotruter = df.rows().map { row ->
             val navn = row["rutenavn"].toString()
-            val geometry: Any = row["geometry"].toString()
+            val geometry: Any = row.get("geometry")!!
             val lengde = row["meter"].toString().toDouble()
             val ruteFølgerString = row["ruteFølger"].toString().replace("[", "").replace("]", "").replace("'", "")
             val ruteFølger = ruteFølgerString.split(", ")
             val merking = row["merking"].toString().toBoolean()
             val skilting = row["skilting"].toString().toBoolean()
             val gradering = row["gradering"].toString()
-            println(row["geometry"].replace())
+            print(geometry)
             Fotrute(navn = navn, geometri = geometry, lengde = lengde, ruteFølger = ruteFølger, merking = merking, skilting = skilting, gradering = gradering )
         }.toMutableList()
     return listofFotruter
