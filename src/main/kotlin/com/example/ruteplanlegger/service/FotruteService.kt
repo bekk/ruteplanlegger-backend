84b78c31994a4e96f3f018a568d54497f21b7ce3
purusott <74315929+purusott@users.noreply.github.com>
2023-09-22 12:54:17 +0200

    Formatert geometri for frontend (#26)
    * get updated csv file of fotruter
    
    * try to change format in csv file to geojson
    
    * convert to geojson from geometrycollection
    
    * formatted geometry for frontend
    
    * update dockerfile to inlcude new CSV to prod
    
    * update rutenavn - to til
    
    * remove commented lines in service
    
    ---------
    
    Co-authored-by: Emilie Lia-Rognli <emilie@bekk-mac-3103.shared>

--- src/main/kotlin/com/example/ruteplanlegger/service/FotruteService.kt
+++ src/main/kotlin/com/example/ruteplanlegger/service/FotruteService.kt
@@ -1,30 +1,59 @@
 package com.example.ruteplanlegger.service
 
 import com.example.ruteplanlegger.model.Fotrute
+import com.example.ruteplanlegger.model.GeoJSONGeometryCollection
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.api.rows
 import org.jetbrains.kotlinx.dataframe.io.read
+import org.jetbrains.kotlinx.dataframe.io.readCSV
 import org.springframework.stereotype.Service
+
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
+import java.io.FileReader
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.locationtech.jts.geom.Geometry
+
 
 @Service
 class FotruteService {
-
     fun getFotruter(): MutableList<Fotrute> {
-        var df = DataFrame.read("fotruter.csv")
-        val listofFotruter = df.rows().map { row ->
-            val navn = row["rutenavn"].toString()
-            val geometry: Any = row["geometry"].toString()
-            val lengde = row["meter"].toString().toDouble()
-            val ruteFølgerString = row["ruteFølger"].toString().replace("[", "").replace("]", "").replace("'", "")
+
+        val reader = FileReader("3-fotruter.csv")
+        val csvParser = CSVParser(reader, CSVFormat.DEFAULT.withHeader())
+
+        val listofFotruter = mutableListOf<Fotrute>()
+
+        for (record in csvParser.records) {
+
+            val navn = record["rutenavn"]
+            val lengde = record["meter"].toDouble()
+            val ruteFølgerString = record["ruteFølger"].toString().replace("[", "").replace("]", "").replace("'", "")
             val ruteFølger = ruteFølgerString.split(", ")
-            val merking = row["merking"].toString().toBoolean()
-            val skilting = row["skilting"].toString().toBoolean()
-            val gradering = row["gradering"].toString()
+            val merking = record["merking"].toBoolean()
+            val skilting = record["skilting"].toBoolean()
+            val gradering = record["gradering"]
 
-            Fotrute(navn = navn, geometri = geometry, lengde = lengde, ruteFølger = ruteFølger, merking = merking, skilting = skilting, gradering = gradering )
-        }.toMutableList()
-    return listofFotruter
+            val geometryJsonString = record["geometry"].toString()
+            val objectMapper = ObjectMapper()
+            val geoJSONFeatureCollection =
+                objectMapper.readValue(geometryJsonString, GeoJSONGeometryCollection::class.java)
 
+            val fotrute = Fotrute(
+                navn = navn,
+                geometri = geoJSONFeatureCollection,
+                lengde = lengde,
+                ruteFølger = ruteFølger,
+                merking = merking,
+                skilting = skilting,
+                gradering = gradering
+            )
+            listofFotruter.add(fotrute)
+        }
+
+        return listofFotruter
+    }
 }
 
-}
