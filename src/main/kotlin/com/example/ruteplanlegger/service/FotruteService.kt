60604473bb5fee0d9ff6d1aa68d2f3278729f72e
Purusoth Thushyanthan <purusoth.thushyanthan@bekk.no>
2023-09-22 12:18:00 +0200

    remove commented lines in service

--- src/main/kotlin/com/example/ruteplanlegger/service/FotruteService.kt
+++ src/main/kotlin/com/example/ruteplanlegger/service/FotruteService.kt
@@ -57,28 +57,3 @@ class FotruteService {
     }
 }
 
-
-/*
-@Service
-class FotruteService {
-
-    fun getFotruter(): MutableList<Fotrute> {
-        var df = DataFrame.read("3-fotruter.csv")
-        val listofFotruter = df.rows().map { row ->
-            val navn = row["rutenavn"].toString()
-            val geometry: Any = row.get("geometry")!!
-            val lengde = row["meter"].toString().toDouble()
-            val ruteFølgerString = row["ruteFølger"].toString().replace("[", "").replace("]", "").replace("'", "")
-            val ruteFølger = ruteFølgerString.split(", ")
-            val merking = row["merking"].toString().toBoolean()
-            val skilting = row["skilting"].toString().toBoolean()
-            val gradering = row["gradering"].toString()
-            print(geometry)
-            Fotrute(navn = navn, geometri = geometry, lengde = lengde, ruteFølger = ruteFølger, merking = merking, skilting = skilting, gradering = gradering )
-        }.toMutableList()
-    return listofFotruter
-
-}
-
-}
-*/
