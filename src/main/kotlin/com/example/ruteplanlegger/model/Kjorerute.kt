9a3f99913cd7868303f181d91f56c36e3ddc51aa
Emilie Lia-Rognli <emilie@bekk-mac-3103.shared>
2023-10-18 11:15:26 +0200

    add descriptive comments to kjorerute fields

--- src/main/kotlin/com/example/ruteplanlegger/model/Kjorerute.kt
+++ src/main/kotlin/com/example/ruteplanlegger/model/Kjorerute.kt
@@ -3,8 +3,9 @@ package com.example.ruteplanlegger.model
 data class Kjorerute(
     val id: String = "",
     val routeName: String = "",
-    val totalDriveTime: Double = 0.0,
-    val totalTime: Double = 0.0, // hva er forskjellen på denne og den over?
+    val totalDriveTime: Double = 0.0, // total drive time of the route, given in minutes (with delays if supported)
+    val totalTime: Double = 0.0, // total time of the route. will mostly be equal to totalDriveTime,
+    // but if delays or certain waiting time restrictions are supported, this will be summarized in totalDriveTime
     val totalLength: Double = 0.0,
     val geojson: GeoJSONFeatureCollection
 )
