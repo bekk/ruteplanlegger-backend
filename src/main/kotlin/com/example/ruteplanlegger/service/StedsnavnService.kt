3841a0d838bd3cb4b9013ce4c24a7a4d5d0f97df
Emilie Lia-Rognli <emilie@bekk-mac-3103.shared>
2023-10-25 14:31:03 +0200

    add api call to adresse api

--- src/main/kotlin/com/example/ruteplanlegger/service/StedsnavnService.kt
+++ src/main/kotlin/com/example/ruteplanlegger/service/StedsnavnService.kt
@@ -6,22 +6,44 @@ import com.fasterxml.jackson.databind.JsonNode
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 
+var index = -1;
 
-fun createStedsnavnObject(data: JsonNode) : List<Stedsnavn> {
-    return data["navn"].mapIndexed {index, sted ->
-        val navn = sted["skrivemåte"].asText()
-        val type = sted["navneobjekttype"].asText()
-        val lat = sted["representasjonspunkt"]["nord"].asDouble()
-        val long = sted["representasjonspunkt"]["øst"].asDouble()
-        Stedsnavn(navn = navn, typeObjekt = type, koordinat = LatLong(lat, long), id = index) }
-
+fun createStedsnavnObject(data: JsonNode) = data["navn"].map { sted ->
+    val navn = sted["skrivemåte"].asText()
+    val type = sted["navneobjekttype"].asText()
+    val lat = sted["representasjonspunkt"]["nord"].asDouble()
+    val long = sted["representasjonspunkt"]["øst"].asDouble()
+    index++
+    Stedsnavn(navn = navn, typeObjekt = type, koordinat = LatLong(lat, long), id = index)
 
 }
+
+fun createStedsnavnObjectFromAdresser(data: JsonNode) = data["adresser"].map { adresse ->
+    val navn = adresse["adressetekst"].asText()
+    val type = adresse["objtype"].asText()
+    val lat = adresse["representasjonspunkt"]["lat"].asDouble()
+    val long = adresse["representasjonspunkt"]["lon"].asDouble()
+    index++
+    Stedsnavn(navn = navn, typeObjekt = type, koordinat = LatLong(lat, long), id = index)
+}
+
 @Service
-class StedsnavnService (val webClient: WebClient.Builder){
-    fun getStedsnavn(query: String): List<Stedsnavn>?{
-        val apiURL = "https://ws.geonorge.no/stedsnavn/v1/navn?sok=$query*&maxAnt=10&antPerSide=10&eksakteForst=true"
-        val response = webClient.baseUrl(apiURL).build().get().retrieve().bodyToMono(JsonNode::class.java).block()
-        return if (response is JsonNode) createStedsnavnObject(response) else emptyList()
+class StedsnavnService(val webClient: WebClient.Builder) {
+    fun getStedsnavn(query: String): List<Stedsnavn>? {
+        val stedsnavnApiURL =
+            "https://ws.geonorge.no/stedsnavn/v1/navn?sok=$query&fuzzy=true&&maxAnt=10&antPerSide=10&eksakteForst=true"
+        val stedsnavnResponse =
+            webClient.baseUrl(stedsnavnApiURL).build().get().retrieve().bodyToMono(JsonNode::class.java).block()
+
+        val adresseApiURL =
+            "https://ws.geonorge.no/adresser/v1/sok?sok=$query&fuzzy=true&treffPerSide=10&asciiKompatibel=true"
+        val adresseResponse =
+            webClient.baseUrl(adresseApiURL).build().get().retrieve().bodyToMono(JsonNode::class.java).block()
+
+        val stedsnavnList = if (stedsnavnResponse is JsonNode) createStedsnavnObject(stedsnavnResponse) else emptyList()
+        val adresseList =
+            if (adresseResponse is JsonNode) createStedsnavnObjectFromAdresser(adresseResponse) else emptyList()
+
+        return stedsnavnList + adresseList
     }
 }
