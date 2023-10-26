66b1f5df6f3e358f9982b26c8250b3c0a4139d26
Emilie Lia-Rognli <emilie@bekk-mac-3103.shared>
2023-10-26 15:30:17 +0200

    change code to follow srp

--- src/main/kotlin/com/example/ruteplanlegger/service/StedsnavnService.kt
+++ src/main/kotlin/com/example/ruteplanlegger/service/StedsnavnService.kt
@@ -3,42 +3,56 @@ package com.example.ruteplanlegger.service
 import com.example.ruteplanlegger.model.LatLong
 import com.example.ruteplanlegger.model.Stedsnavn
 import com.fasterxml.jackson.databind.JsonNode
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 
-fun createStedsnavnObjectFromStedsnavnAPI(data: JsonNode) = data["navn"].map { sted ->
-    val navn = sted["skrivemåte"].asText()
-    val type = sted["navneobjekttype"].asText()
-    val lat = sted["representasjonspunkt"]["nord"].asDouble()
-    val long = sted["representasjonspunkt"]["øst"].asDouble()
-    Stedsnavn(navn = navn, typeObjekt = type, koordinat = LatLong(lat, long))
-
-}
-
-fun createStedsnavnObjectFromAdresserAPI(data: JsonNode) = data["adresser"].map { adresse ->
-    val navn = adresse["adressetekst"].asText()
-    val type = adresse["objtype"].asText()
-    val lat = adresse["representasjonspunkt"]["lat"].asDouble()
-    val long = adresse["representasjonspunkt"]["lon"].asDouble()
-    Stedsnavn(navn = navn, typeObjekt = type, koordinat = LatLong(lat, long))
-}
-
 @Service
-class StedsnavnService(val webClient: WebClient.Builder) {
-    fun getStedsnavn(query: String): List<Stedsnavn>? {
+class ApiService(val webClient: WebClient.Builder) {
+    fun getStedsnavnDataFromAPI(query: String): JsonNode? {
         val stedsnavnApiURL =
             "https://ws.geonorge.no/stedsnavn/v1/navn?sok=$query&fuzzy=true&&maxAnt=10&antPerSide=10&eksakteForst=true"
         val stedsnavnResponse =
             webClient.baseUrl(stedsnavnApiURL).build().get().retrieve().bodyToMono(JsonNode::class.java).block()
-
+        return stedsnavnResponse
+    }
+    fun getAdresseDataFromAPI(query: String): JsonNode? {
         val adresseApiURL =
             "https://ws.geonorge.no/adresser/v1/sok?sok=$query&fuzzy=true&treffPerSide=10&asciiKompatibel=true"
         val adresseResponse =
             webClient.baseUrl(adresseApiURL).build().get().retrieve().bodyToMono(JsonNode::class.java).block()
+        return adresseResponse
+    }
+}
 
-        val stedsnavnList = if (stedsnavnResponse is JsonNode) createStedsnavnObjectFromStedsnavnAPI(stedsnavnResponse) else emptyList()
+@Component
+class StedsnavnParser {
+    fun parseStedsnavnFromStedsnavnAPI(data: JsonNode) = data["navn"].map { sted ->
+        val navn = sted["skrivemåte"].asText()
+        val type = sted["navneobjekttype"].asText()
+        val lat = sted["representasjonspunkt"]["nord"].asDouble()
+        val long = sted["representasjonspunkt"]["øst"].asDouble()
+        Stedsnavn(navn = navn, typeObjekt = type, koordinat = LatLong(lat, long))
+    }
+
+    fun parseStedsnavnFromAdresserAPI(data: JsonNode) = data["adresser"].map { adresse ->
+        val navn = adresse["adressetekst"].asText()
+        val type = adresse["objtype"].asText()
+        val lat = adresse["representasjonspunkt"]["lat"].asDouble()
+        val long = adresse["representasjonspunkt"]["lon"].asDouble()
+        Stedsnavn(navn = navn, typeObjekt = type, koordinat = LatLong(lat, long))
+    }
+}
+
+@Service
+class StedsnavnService(val apiService: ApiService, val stedsnavnParser: StedsnavnParser) {
+    fun getStedsnavn(query: String): List<Stedsnavn>? {
+        val stedsnavnResponse = apiService.getStedsnavnDataFromAPI(query)
+        val adresseResponse = apiService.getAdresseDataFromAPI(query)
+
+        val stedsnavnList = if (stedsnavnResponse is JsonNode) stedsnavnParser.parseStedsnavnFromStedsnavnAPI(stedsnavnResponse) else emptyList()
         val adresseList =
-            if (adresseResponse is JsonNode) createStedsnavnObjectFromAdresserAPI(adresseResponse) else emptyList()
+            if (adresseResponse is JsonNode) stedsnavnParser.parseStedsnavnFromAdresserAPI(adresseResponse) else emptyList()
 
         return stedsnavnList + adresseList
     }
