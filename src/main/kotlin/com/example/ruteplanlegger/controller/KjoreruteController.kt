bfa1b4fda0340cdfb1e7ef67f056119cd0909cb2
Purusoth Thushyanthan <purusoth.thushyanthan@bekk.no>
2023-11-15 10:54:57 +0100

    add exception handling and refactor similar code

--- src/main/kotlin/com/example/ruteplanlegger/controller/KjoreruteController.kt
+++ src/main/kotlin/com/example/ruteplanlegger/controller/KjoreruteController.kt
@@ -3,6 +3,8 @@ package com.example.ruteplanlegger.controller
 import com.example.ruteplanlegger.model.Kjorerute
 import com.example.ruteplanlegger.model.LatLong
 import com.example.ruteplanlegger.service.KjoreruteService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -12,15 +14,30 @@ import org.springframework.web.bind.annotation.*
 class KjoreruteController(val kjoreruteService: KjoreruteService) {
 
     @GetMapping
-    fun getKjoreruter(@RequestParam start: List<Double>, @RequestParam slutt: List<Double>):  Kjorerute? {
-        return kjoreruteService.getKjoreruter(start, slutt)
+    @CrossOrigin(origins = ["*"])
+    fun getKjoreruter(@RequestParam start: List<Double>, @RequestParam slutt: List<Double>):  ResponseEntity<Kjorerute>? {
+        try {
+            val res =  kjoreruteService.getKjoreruter(start, slutt)
+            return ResponseEntity.ok(res)
+        }
+        catch (ex: Exception) {
+            return ResponseEntity.badRequest().build()
+        }
     }
 
 
     @GetMapping
     @RequestMapping("/tourist")
-    fun getTouristKjoreruter(@RequestParam start: List<Double>, @RequestParam slutt: List<Double>):  Kjorerute? {
-        return kjoreruteService.getTouristKjoreruter(start, slutt)
+    @CrossOrigin(origins = ["*"])
+    fun getTouristKjoreruter(@RequestParam start: List<Double>, @RequestParam slutt: List<Double>):  ResponseEntity<Kjorerute>? {
+        try {
+            val res = kjoreruteService.getTouristKjoreruter(start, slutt)
+            return ResponseEntity.ok(res)
+        }
+        catch (ex: Exception) {
+            return ResponseEntity.badRequest().build()
+        }
+
     }
 
 }
